bdf25d22a31cccee622c7bd1783fd771cf2fbdf7
Smit Lakhani <smitlakhani13@gmail.com>
2023-01-13 04:06:32 +0530

    stepping more

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int currentIndex = 0;
   final screens = [
-    HomePage(),
+    MainPage(),
     SearchPage(),
   ];
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,45 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final urlImages = [
+    'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg'
+        'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg'
+        'https://terrigen-cdn-dev.marvel.com/content/prod/1x/theavengers_lob_crd_03_0.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('Home Page'),
-        ),
         body: Center(
-            child: Text(
-          'Home',
-          style: TextStyle(fontSize: 60),
+            child: CarouselSlider.builder(
+          options: CarouselOptions(
+              height: 300,
+              // viewportFraction: 1,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              enlargeStrategy: CenterPageEnlargeStrategy.height,
+              // reverse: true,
+              autoPlayInterval: Duration(seconds: 2)),
+          itemCount: urlImages.length,
+          itemBuilder: (context, index, realIndex) {
+            final urlImage = urlImages[index];
+
+            return buildImage(urlImage, index);
+          },
         )),
+      );
+
+  Widget buildImage(String urlImage, int index) => Container(
+        margin: EdgeInsets.symmetric(horizontal: 12),
+        color: Colors.grey,
+        child: Image.network(
+          urlImage,
+          fit: BoxFit.cover,
+        ),
       );
 }
