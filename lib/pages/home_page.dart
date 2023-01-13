fd1319e50102eb1a1246605b649bfa9fb7c63f10
Smit Lakhani <smitlakhani13@gmail.com>
2023-01-14 00:30:36 +0530

    Home Screen Updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,53 @@ import 'package:flutter/material.dart';
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        margin: const EdgeInsets.only(top: 80),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Latest Movies",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    "See All",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(children: [
+                for (int i = 1; i < 7; i++)
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        "assets/images/$i.jpg",
+                        height: 180,
+                        width: 300,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+              ]),
+            )
+          ],
+        ));
   }
 }
